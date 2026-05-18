ee3167561eefa5240e7bd2beb9231f3aab57e6fc
Emtiaz Hossain Sami <samiemtiaz38@gmail.com>
2026-05-18 23:36:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const animalData = {
     },
     camel: {
         text: "🐪 Camel: Eid Mubarak! Warm wishes from the desert camel!",
-        audioFile: "voice_preview_camel.mp3" // সিঙ্গেল .mp3 করা হলো
+        audioFile: "voice_preview_camel.mp3.mp3" // সিঙ্গেল .mp3 করা হলো
     },
     cow: {
         text: "🐄 Cow: Moo Moo! Eid Mubarak! May your sacrifices be accepted.",
